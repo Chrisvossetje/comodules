6bf83412bd450ce1b57cee0ae16da0e6b7449b92
Chris Vos <chris@bitfox.nl>
2024-11-28 18:48:44 +0100

    Simpler

--- src/resolution.rs
+++ src/resolution.rs
@@ -1,13 +1,13 @@
-use crate::comodule::{SimpleComodule, SimpleComoduleMorphism};
+use crate::comodule::{Comodule, ComoduleMorphism};
 
 
-pub struct Resolution<M: SimpleComodule, F: SimpleComoduleMorphism<M>> {
+pub struct Resolution<M: Comodule, F: ComoduleMorphism<M>> {
     comodule: M,
     resolution: Vec<F>,
 }
 
 
-impl<M: SimpleComodule, F: SimpleComoduleMorphism<M>> Resolution<M, F>{
+impl<M: Comodule, F: ComoduleMorphism<M>> Resolution<M, F>{
     pub fn new(comodule: M) -> Self {
         Resolution {
             comodule,
@@ -23,7 +23,7 @@ impl<M: SimpleComodule, F: SimpleComoduleMorphism<M>> Resolution<M, F>{
         
         for i in (0..s) {
             let last_morph = self.resolution.last().unwrap();
-            
+
             let coker = last_morph.cokernel();
 
             let inject = last_morph.injection_codomain_to_cofree();
